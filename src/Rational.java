660ca2a37972861420f598cedfc4fa19cbac342b
Dominic Kirby <dkirby@umail.ucsb.edu>
2016-10-04 15:55:48 -0700

    trying the fix again

--- src/Rational.java
+++ src/Rational.java
@@ -74,12 +74,13 @@ public class Rational {
 	if(this.num == 0){
 	    throw new ArithmeticException("Numerator Can't Be Zero");
 	}
-	if(this.num <0)
+	Rational ans = new Rational(this.denom, this.num);
+	if(ans.denom <0)
 	    {
 		this.num = this.num * -1;
 		this.denom = this.denom * -1;
 	    }
-       	return new Rational(this.denom, this.num);
+       	return ans;
         
     }
 
